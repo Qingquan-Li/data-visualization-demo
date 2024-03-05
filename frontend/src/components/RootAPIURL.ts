172ef8f65427e7a0d2f5716a204a76471ff82227
Qingquan Li <liqqisme@gmail.com>
2024-03-05 15:14:14 -0500

    Replace Docker Compose with Kubernetes to deploy containers

--- frontend/src/components/RootAPIURL.ts
+++ frontend/src/components/RootAPIURL.ts
@@ -1,3 +1,2 @@
 // export const RootAPIURL: string = "http://localhost:8080/api"; // For local development
 export const RootAPIURL: string = 'http://localhost:30010/api'; // For Kubernetes
-// export const RootAPIURL: string = 'https://data-visualization-demo.qingquanli.com/api';
